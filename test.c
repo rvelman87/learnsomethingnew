c63cdc2b10588c1497dfc46602117b7d53d5643d
rvelman87 <61086924+rvelman87@users.noreply.github.com>
2022-10-05 09:01:03 -0400

    Add files via upload

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    char str1 = "7MSGTYPE8LOCATION7TRAM_ID7TRAMABC5VALUE4CITY";
+    char str2 = "7MSGTYPEFPASSENGER_COUNT7TRAM_ID7TRAMABC";
+    int idx = 0;
+
+    return 0;
+}
